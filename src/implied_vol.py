739cd2415c716bb8b11bd8f69586597779013f92
iandailey <iandailey16@gmail.com>
2026-06-19 15:15:40 -0400

    feat: add function to compute IV

--- src/implied_vol.py
+++ src/implied_vol.py
@@ -15,7 +15,22 @@ def bsm_prices(S, K, T, r, sigma, option_type):
         return put
     else:
         raise ValueError(f"option_type much be 'call' or 'put', got {option_type}")
+
+def compute_implied_vol(market_price, S, K, T, r, option_type):
+
+    def objective(sigma):
+        return bsm_prices(S, K, T, r, sigma, option_type) - market_price
+
     
+
+    try:
+        sigma = brentq(objective, 1e-6, 5.0)
+        return sigma
+    except:
+        return np.nan
+
+
+
 if __name__ == "__main__":
     price = bsm_prices(S=100, K=100, T=0.5, r=0.05, sigma=0.2, option_type='call')
     print(f"Call price: {price:.2f}")
